74d9d93f4b1a1b6a81c52b907d1635699deb1aca
Chad L <72481913+clookcode@users.noreply.github.com>
2020-10-10 11:07:35 -0400

    adding my hello line

--- src/W04_Prove/Main.java
+++ src/W04_Prove/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // original line
         System.out.println("Hello, my name is Nathan");
-
+        // additional line from Chad
+        System.out.println("Hello Nathan, my name is Chad.");
     }
 }
